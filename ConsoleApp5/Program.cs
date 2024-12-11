025b6fddce21f2afc8addb3066274590d0b3e089
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-11 09:14:28 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static int Centimeters(int m, int c)
+        {
+            return m * 100 + c;
+        }
+        static void Main(string[] args)
+        {
+            int meters = 15;
+            int centimeters = 27;
+            int totalCentimeters = Centimeters(meters, centimeters);
+            Console.ReadKey(); 
+        }
+    }
+}
