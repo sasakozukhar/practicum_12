c915269f9079562587880be130dc4fbc348d15ff
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-11 09:17:55 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static int Kop(int r, int k)
+        {
+            return r * 100 + k;
+        }
+        static void Main(string[] args)
+        {
+            int rubles1 = 5;
+            int kopecks1 = 24;
+            int totalKopecks1 = Kop(rubles1, kopecks1);
+            Console.WriteLine($"Количество копеек в 5 руб. 24 коп.: {totalKopecks1}");
+
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static int Kop(int r, int k)
+        {
+            return r * 100 + k;
+        }
+        static void Main(string[] args)
+        {
+            int rubles2 = 15;
+            int kopecks2 = 7;
+            int totalKopecks2 = Kop(rubles2, kopecks2);
+            Console.WriteLine($"Количество копеек в 15 руб. 7 коп.: {totalKopecks2}");
+
+        }
+    }
+}
