134b2deeaad0c8dde7c1c28f75445dfa1d8bd559
sasakozukhar <sasakozuhar93@gmail.com>
2024-12-11 09:11:10 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static int Centimeters(int m, int c)
+        {
+            return m * 100 + c;
+        }
+        static void Main(string[] args)
+        {
+            int meters = 2;
+            int centimeters = 30;
+            int totalCentimeters = Centimeters(meters, centimeters);
+            Console.WriteLine($"Количество сантиметров: {totalCentimeters}");
+            Console.ReadKey();
+        }
+    }
+}
